ec9a33b03d18c41fd09ab47802fe78ade68bbb9f
Salma_Ahmed <107878740+Salma55655@users.noreply.github.com>
2023-04-08 11:56:06 +0200

    Update Categories.jsx

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -14,7 +14,7 @@ const Categories = () => {
 
   return (
     <div className="bg-[#ECF5F2] shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">فئات</h3>
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`}>
           <span
