d3c2c742a8792d6e509059aad6bd00f7e9667fb7
Salma_Ahmed <107878740+Salma55655@users.noreply.github.com>
2023-04-16 02:58:28 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,39 +9,44 @@ import Face from './Face'
 const Footer = () => {
 
   return (
-    <footer className="mt-100 px-10 py-3 bg-[#CDE2D9] flex items-center justify-between text-sm">
-      <div className="flex items-center space-between">
-        <Image src={Logo} alt="" height="50" />
-        <span className="text-base font-semibold">Contact us</span>
+    <footer className="px-10 py-3 bg-[#CDE2D9] flex flex-col lg:flex-row items-center justify-between text-sm ">
+      <div className="flex flex-col lg:flex-row items-center space-between mb-4 lg:mb-0 md:mb-5">
         <div className="item-icon">
           <a
             href="https://www.facebook.com/profile.php?id=100087454669404"
-            target="blank"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <Face className="fab" />
-            {/* <FontAwesomeIcon icon={faFacebook} className="fab" /> */}
           </a>
         </div>
+        <Image src={Logo} alt="" height="50" />
+        <span className="text-base font-semibold ml-4 lg:ml-0 lg:mr-4">
+          Contact us
+        </span>
       </div>
-      <div className="flex items-center">
-        <div className="item-icon">
-          <a href="https://www.facebook.com/profile.php?id=100080322847763&sk=about">
-            <Face className="fab" />
-
-            {/* <FontAwesomeIcon icon={faLinkedInIn} className="fab" /> */}
-          </a>
-        </div>
-        <span className=" cursor-pointer hover:text-[#EF8A29] text-base">
+      <div className="flex flex-col lg:flex-row items-center">
+        <span className=" cursor-pointer hover:text-[#EF8A29] text-base mb-4 sm:mb-0 lg:mb-0 mr-4">
           ♥️ Made by ♥️
           <Link
             className="link "
             href="https://www.facebook.com/profile.php?id=100080322847763&sk=about"
-            target="blank"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             {" "}
             <b>Salma</b>{" "}
           </Link>
         </span>
+        <div className="item-icon ml-4">
+          <a
+            href="https://www.facebook.com/profile.php?id=100080322847763&sk=about"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Face className="fab" />
+          </a>
+        </div>
       </div>
     </footer>
   );
